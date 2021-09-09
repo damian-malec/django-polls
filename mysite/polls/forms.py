67d88da40586182059f62ad5d73ddad378c1242e
dmaleccs <damian.malec@cloudservices.pl>
2021-09-09 14:39:57 +0200

    Ankiety Django - forms.py

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -8,20 +8,19 @@ class CommentForm(forms.ModelForm):
         fields = ('autor', 'tekst',)
 
 
-class ChoiceForm(forms.ModelForm):
+class ChoiceForm(forms.Form):
     choice_text = forms.ChoiceField(choices=(
         ('1', 'opcja 1'),
         ('2', 'opcja 2'),
         ('3', 'opcja 3'),
     ),
-        widget=forms.RadioSelect(attrs={'id': 'value'})
+        widget=forms.RadioSelect(attrs={'class': 'star'})
     )
 
     class Meta:
         model = Choice
         fields = ('choice_text',)
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.fields['choice_text'].choices = [('1','xxx')]
-            breakpoint()
+        # def __init__(self, *args, **kwargs):
+        #    super().__init__(*args, **kwargs)
+        #    self.fields['choice_text'].choices = [('1','xxx')]
